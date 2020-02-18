07c2e2f1f350d40ce23ebcd98bb6d365e789d642
bruniinha1805 <61169669+bruniinha1805@users.noreply.github.com>
2020-02-18 17:45:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const nome = prompt("Coloque seu nome")
+const res = document.getElementById("nome")
+res.innerHTML = `Olá, ${nome}!`
+const desejo = confirm("Deseja fazer a prova?")
+if (desejo == false) {
+const n = document.getElementById("desejo")
+n.innerHTML = `Prova cancelada com sucesso!`
+}
+else {
+const um = prompt("Pergunda 1: Quanto é 2 + 2? \n 1| 10 \n 2| 8 \n 3| 4 \n 4| 2")
+}
+
+
+ 
+

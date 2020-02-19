1e815a4afa69c923d11e02865a634efd42a79413
AlineSanqueza <61170752+AlineSanqueza@users.noreply.github.com>
2020-02-19 00:14:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const res = document.getElementById("nome")
 res.innerHTML = `Olá, ${nome}!`
 const desejo = confirm("Deseja fazer a prova?")
 if (desejo == false) {
-const n = document.getElementById("desejo")
+const cancel = document.getElementById("desejo")
 n.innerHTML = `Prova cancelada com sucesso!`
 }
 else {
-const um = prompt("Pergunda 1: Quanto é 2 + 2? \n 1| 10 \n 2| 8 \n 3| 4 \n 4| 2")
+const p1 = prompt("Pergunda 1: Quanto é 2 + 2? \n 1| 10 \n 2| 8 \n 3| 4 \n 4| 2")
 }
 
 
